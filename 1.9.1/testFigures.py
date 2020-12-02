92f7f13579c10f4e6dec380927141e4b124a67d9
Nikita Borisov <nborisov256@gmail.com>
2020-12-02 22:42:37 +0300

    1.9.1

---
+++ 1.9.1/testFigures.py
@@ -0,0 +1,20 @@
+from figures import Rectangle, Square, Circle
+
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+circle_1 = Circle(3)
+circle_2 = Circle(4)
+
+figures = [rect_1, rect_2, square_1, square_2, circle_1, circle_2]
+
+for figure in figures:
+    if isinstance(figure, Square):
+        print('Площадь квадрата:', figure.get_area_square())
+    elif isinstance(figure, Rectangle):
+        print('Площадь прямоугольника:', figure.get_area())
+    else:
+        print('Площадь круга:', figure.get_area_circle())
