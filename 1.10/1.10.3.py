8d5cb3e22a626c2d63690067f91beae2571ea7d4
Nikita Borisov <nborisov256@gmail.com>
2020-12-03 14:19:50 +0300

    Update 1.10.3.py

--- 1.10/1.10.3.py
+++ 1.10/1.10.3.py
@@ -7,6 +7,10 @@ class EWallet:
     def get_balance(self):
         return self.balance
 
+    def set_balance(self, balance):
+        if balance >= 0 and isinstance(balance, int):
+            self.balance = balance
+
 
 name = input('Введите имя: ')
 surname = input('Введите фамилию: ')
@@ -15,6 +19,7 @@ while not balance.isdigit():
     balance = input('Сумма должна быть числом, повторите ввод: ')
 balance = int(balance)
 
-client = EWallet(name, surname, balance)
+client = EWallet(name, surname)
+client.set_balance(balance)
 
-print(('Клиент %s %s. Баланс: %s руб.') % (client.name, client.surname, client.balance))
+print(('Клиент %s %s. Баланс: %s руб.') % (client.name, client.surname, client.get_balance()))
